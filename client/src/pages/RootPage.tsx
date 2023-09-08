c1d449faa5a9d694cd42680204582a2c349cc9a3
Mika <sonicyuri@protonmail.com>
2023-09-08 01:00:35 -0400

    Fix double auto-login

--- client/src/pages/RootPage.tsx
+++ client/src/pages/RootPage.tsx
@@ -1,15 +1,42 @@
 /** @format */
 import { Box, Paper } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 
 import MenuBar from "../components/MenuBar";
-import { selectAuthState } from "../features/auth/AuthSlice";
+import { loginToken, refresh, selectAuthState } from "../features/auth/AuthSlice";
 import { useAppDispatch, useAppSelector } from "../features/Hooks";
+import { LocalSettings } from "../util/LocalSettings";
+import { Util } from "../util/Util";
 import LoginPage from "./LoginPage";
 
+let alreadyAutoLoggedInFlag = false;
+
 const RootPage = () => {
-	const { isLoggedIn, user } = useAppSelector(selectAuthState);
+	const dispatch = useAppDispatch();
+	const { isLoggedIn, user, loginState } = useAppSelector(selectAuthState);
+
+	useEffect(() => {
+		if (loginState != "initial" || alreadyAutoLoggedInFlag) {
+			return;
+		}
+
+		if (LocalSettings.accessToken.value && Util.checkIfTokenValid(LocalSettings.accessTokenExpire.value ?? "")) {
+			alreadyAutoLoggedInFlag = true;
+			dispatch(
+				loginToken({
+					accessToken: LocalSettings.accessToken.value,
+					refreshToken: LocalSettings.refreshToken.value
+				})
+			);
+		} else if (
+			LocalSettings.refreshToken.value &&
+			Util.checkIfTokenValid(LocalSettings.refreshTokenExpire.value ?? "")
+		) {
+			alreadyAutoLoggedInFlag = true;
+			dispatch(refresh(LocalSettings.refreshToken.value));
+		}
+	}, []);
 
 	const body = (
 		<>
