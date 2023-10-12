6126e30b48a14aa8769a7f66307b2a6114bf3ae5
Mika <sonicyuri@protonmail.com>
2023-10-12 08:09:32 -0400

    Favorites set endpoint

--- server-rs/src/modules/favorites/api.rs
+++ server-rs/src/modules/favorites/api.rs
@@ -1,9 +1,27 @@
-use actix_web::{get, web, HttpRequest, HttpResponse};
+use actix_web::{get, post, web, HttpRequest, HttpResponse};
 use log::error;
+use sqlx::MySqlPool;
 
 use crate::modules::auth::middleware::{get_user, AuthFactory};
-use crate::util::{api_error, api_success};
+use crate::util::{api_error, api_success, format_db_error};
 use crate::{util::ApiError, AppState};
+
+use super::schema::{FavoriteAction, FavoriteSetSchema};
+
+pub async fn get_favorites(user_id: i32, db: &MySqlPool) -> Result<Vec<String>, ApiError> {
+    let query_result = sqlx::query!(
+        r#"SELECT image_id FROM user_favorites WHERE user_id = ?"#,
+        user_id
+    )
+    .fetch_all(db)
+    .await
+    .map_err(format_db_error)?;
+
+    Ok(query_result
+        .iter()
+        .map(|v| v.image_id.to_string())
+        .collect())
+}
 
 #[get("/list", wrap = "AuthFactory { reject_unauthed: true }")]
 pub async fn favorites_list_handler(
@@ -15,24 +33,55 @@ pub async fn favorites_list_handler(
         "Missing user",
     ))?;
 
-    let query_result = sqlx::query!(
-        r#"SELECT image_id FROM user_favorites WHERE user_id = ?"#,
-        user.id
-    )
-    .fetch_all(&data.db)
-    .await
-    .map_err(|e| {
-        error!("Database error: {:?}", e);
-        api_error(
-            crate::util::ApiErrorType::ServerError,
-            "Unknown database error",
-        )
-    })?;
+    Ok(api_success(get_favorites(user.id, &data.db).await?))
+}
 
-    let ids: Vec<String> = query_result
-        .iter()
-        .map(|v| v.image_id.to_string())
-        .collect();
+#[post("/set", wrap = "AuthFactory { reject_unauthed: true }")]
+pub async fn favorites_set_handler(
+    req: HttpRequest,
+    data: web::Data<AppState>,
+    body: web::Json<FavoriteSetSchema>,
+) -> Result<HttpResponse, ApiError> {
+    let user = get_user(&req).ok_or(api_error(
+        crate::util::ApiErrorType::AuthorizationFailed,
+        "Missing user",
+    ))?;
 
-    Ok(api_success(ids))
+    let query_result =
+        sqlx::query_scalar!("SELECT COUNT(id) FROM images WHERE id = ?", body.postId)
+            .fetch_one(&data.db)
+            .await
+            .map_err(format_db_error)?;
+
+    if query_result < 1 {
+        return Err(api_error(
+            crate::util::ApiErrorType::InvalidRequest,
+            "Post not found",
+        ));
+    }
+
+    match body.action {
+        FavoriteAction::Set => {
+            sqlx::query!(
+                "INSERT IGNORE INTO user_favorites SET user_id = ?, image_id = ?",
+                user.id,
+                body.postId
+            )
+            .execute(&data.db)
+            .await
+            .map_err(format_db_error)?;
+        }
+        FavoriteAction::Unset => {
+            sqlx::query!(
+                "DELETE FROM user_favorites WHERE user_id = ? AND image_id = ?",
+                user.id,
+                body.postId
+            )
+            .execute(&data.db)
+            .await
+            .map_err(format_db_error)?;
+        }
+    };
+
+    Ok(api_success(get_favorites(user.id, &data.db).await?))
 }
--- server-rs/src/util.rs
+++ server-rs/src/util.rs
@@ -1,5 +1,6 @@
 use actix_web::{http::StatusCode, HttpResponse};
 use derive_more::{Display, Error};
+use log::error;
 use serde::Serialize;
 use serde_json::json;
 
@@ -16,13 +17,13 @@ pub enum ApiErrorType {
 #[derive(Debug, Display, Error, Clone)]
 #[display(fmt = "API error: {}", message)]
 pub struct ApiError {
-    message: &'static str,
+    message: String,
     error_type: ApiErrorType,
 }
 
 impl actix_web::error::ResponseError for ApiError {
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
-        HttpResponse::build(self.status_code()).json(error_response(self.message))
+        HttpResponse::build(self.status_code()).json(error_response(self.message.clone()))
     }
 
     fn status_code(&self) -> StatusCode {
@@ -38,13 +39,20 @@ impl actix_web::error::ResponseError for ApiError {
 }
 
 pub fn unauthorized_error(message: &str) -> HttpResponse {
-    HttpResponse::Unauthorized().json(error_response(message))
+    HttpResponse::Unauthorized().json(error_response(message.to_owned()))
 }
 
 pub fn api_error(error_type: ApiErrorType, message: &'static str) -> ApiError {
     ApiError {
         error_type,
+        message: message.to_owned(),
+    }
+}
+
+pub fn api_error_owned(error_type: ApiErrorType, message: String) -> ApiError {
+    ApiError {
         message,
+        error_type,
     }
 }
 
@@ -56,6 +64,14 @@ pub fn success_response(value: impl Serialize) -> impl Serialize {
     json!({ "type": "success", "result": value })
 }
 
-pub fn error_response(message: &str) -> impl Serialize {
+pub fn error_response(message: String) -> impl Serialize {
     json!({ "type": "error", "message": message })
+}
+
+pub fn format_db_error(err: sqlx::error::Error) -> ApiError {
+    error!("Database error: {:?}", err);
+    api_error(
+        crate::util::ApiErrorType::ServerError,
+        "Unknown database error",
+    )
 }
