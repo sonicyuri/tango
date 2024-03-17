248c90aed0613194ddc47eb66ec75cf2718dd399
Mika <sonicyuri@protonmail.com>
2024-03-17 17:43:53 -0400

    Refactor invites

--- client/src/pages/user/components/InviteList.tsx
+++ client/src/pages/user/components/InviteList.tsx
@@ -1,28 +1,41 @@
-import { Button, IconButton, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
+/** @format */
+
+import {
+	Button,
+	IconButton,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableFooter,
+	TableHead,
+	TableRow
+} from "@mui/material";
 import { useEffect } from "react";
 import LoadingSpinner from "../../../components/LoadingSpinner";
 import { useAppDispatch, useAppSelector } from "../../../features/Hooks";
-import { inviteCreate, inviteDelete, inviteList, selectInviteState } from "../../../features/invites/InviteSlice";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import {
+	inviteCreate,
+	inviteDelete,
+	inviteList,
+	selectInviteState
+} from "../../../features/invites/InviteSlice";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import moment from "moment";
 
-const InviteList = () =>
-{
-	const { invites, loadingState } = useAppSelector(selectInviteState);
+const InviteList = () => {
+	const { invites } = useAppSelector(selectInviteState);
 	const dispatch = useAppDispatch();
 
-	const handleGenerateInvite = () =>
-	{
+	const handleGenerateInvite = () => {
 		dispatch(inviteCreate(null));
 	};
 
-	const handleDeleteInvite = (invite_id: string) =>
-	{
+	const handleDeleteInvite = (invite_id: string) => {
 		dispatch(inviteDelete(invite_id));
-	}
+	};
 
-	useEffect(() =>
-	{
+	useEffect(() => {
 		dispatch(inviteList(null));
 	}, []);
 
@@ -43,35 +56,67 @@ const InviteList = () =>
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{loadingState == "loading" ? <LoadingSpinner /> : invites.map(invite => {
-						return (
-							<TableRow hover role="checkbox" tabIndex={-1} key={"invite-" + invite.id}>
-								<TableCell align="left">{invite.invite_code}</TableCell>
-								<TableCell align="left" style={{fontStyle: "italic"}}>
-									{invite.redeemed ? "Redeemed " + moment(invite.redeemed_time).format("dddd, MMMM Do YYYY, h:mm:ss a") : "Unused"}
-								</TableCell>
-								<TableCell align="left">
-									<IconButton aria-label="delete invite" onClick={() => handleDeleteInvite(invite.id)}>
-										<DeleteForeverIcon />
-									</IconButton>
-								</TableCell>
-							</TableRow>
-						);
-					})}
+					{invites.ready() ? (
+						<LoadingSpinner />
+					) : (
+						invites.value.map(invite => {
+							return (
+								<TableRow
+									hover
+									role="checkbox"
+									tabIndex={-1}
+									key={"invite-" + invite.id}>
+									<TableCell align="left">
+										{invite.invite_code}
+									</TableCell>
+									<TableCell
+										align="left"
+										style={{ fontStyle: "italic" }}>
+										{invite.redeemed
+											? "Redeemed " +
+												moment(
+													invite.redeemed_time
+												).format(
+													"dddd, MMMM Do YYYY, h:mm:ss a"
+												)
+											: "Unused"}
+									</TableCell>
+									<TableCell align="left">
+										<IconButton
+											aria-label="delete invite"
+											onClick={() =>
+												handleDeleteInvite(invite.id)
+											}>
+											<DeleteForeverIcon />
+										</IconButton>
+									</TableCell>
+								</TableRow>
+							);
+						})
+					)}
 				</TableBody>
 				<TableFooter>
 					<TableRow>
 						<TableCell />
 						<TableCell />
 						<TableCell>
-							<Button fullWidth color="primary" variant="contained" onClick={handleGenerateInvite}>
-								{loadingState == "loading" ? <LoadingSpinner /> : <span>Generate Invite</span>}
+							<Button
+								fullWidth
+								color="primary"
+								variant="contained"
+								onClick={handleGenerateInvite}>
+								{invites.ready() ? (
+									<LoadingSpinner />
+								) : (
+									<span>Generate Invite</span>
+								)}
 							</Button>
 						</TableCell>
 					</TableRow>
 				</TableFooter>
 			</Table>
-		</TableContainer>);
-}
+		</TableContainer>
+	);
+};
 
 export default InviteList;
